10f32e533d57f784283bd9b8d7e5fe72aae0209f
NamCheolUng <nam92116@naver.com>
2021-08-25 17:51:51 +0900

    Update list.php

--- board/list.php
+++ board/list.php
@@ -8,14 +8,18 @@
 </head>
 <body>
 	<?php
+    // if(!$_GET['page']) $page = 1;
+    // $page = $_GET['page'];
+
     $conn = mysqli_connect("localhost", "root", "111111", "bo_table");
 
     $sql = "SELECT * FROM board";
+    
     $result = mysqli_query($conn, $sql);
 
    ?>
    <h1>자유게시판</h1>
-<table border="1">
+    <table border="1">
     	<thead>
     		<tr>
     			<th>No</th>
@@ -27,10 +31,10 @@
     	<?php
     while ($row=mysqli_fetch_array($result)) {
     ?>
-    <tbody>
+        <tbody>
        		<tr>
     			<td><?=$row['number']?></td>
-    			<td><a href="view.php?number=<?=$row['number']?>"><?=$row['title']?></a></td>
+                <td><a href="view.php?number=<?=$row['number']?>"><?=$row['title']?></a></td>
     			<td><?=$row['name']?></td>
     			<td><?=$row['created']?></td>
     		</tr>
@@ -39,7 +43,28 @@
 	}
 	  mysqli_close($conn);
 ?>
-</table>
- <button type = "button" onclick="location.href='write.html'">글쓰기</button>
+    </table>
+     <div>
+        <button type = "button" onclick="location.href='write.html'">글쓰기</button>
+     </div>
+     <!--
+     <ul>
+        <li><a href="#">prev</a></li>
+        <li><a href="#">1</a></li>
+        <li><a href="#">2</a></li>
+        <li><a href="#">next</a></li>
+    </ul>
+    -->
+        <div>
+        <form action="search.php" method="get">
+            <select name="sKey">
+                <option value="0" selected>선택</option>
+                <option value="title">제목</option>
+                <option value="name">글쓴이</option>
+            </select>
+            <input type="text" name="sText">
+            <input type="submit" value="검색">
+         </form>
+        </div>
 </body>
 </html>
