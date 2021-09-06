dc333201090c2f95e3f7a52a74ee8d29a8623cc1
NamCheolUng <nam92116@naver.com>
2021-09-06 16:38:54 +0900

    Update view.php

--- board/view.php
+++ board/view.php
@@ -1,42 +1,43 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>상세보기</title>
-</head>
-<body>
-	<?php
+<?php
+    include 'header.php';
+
     $conn = mysqli_connect("localhost", "root", "111111", "bo_table");
 
- 	$view_num = $_GET['number'];
+ 	 $view_num = $_GET['number'];
+    
+    $sql1 = "UPDATE board SET hit = hit + 1 WHERE number = '$view_num'";
+    mysqli_query($conn, $sql1);
 
- 	$sql = "SELECT * FROM board WHERE number = '$view_num'";
+    $sql = "SELECT * FROM board WHERE number = '$view_num'";
+
+
  	$result = mysqli_query($conn, $sql);
- 	?>
- <h1>상세보기</h1>
-    <?php
+?>
+  <h1>상세보기</h1>
+<?php
     if ($row=mysqli_fetch_array($result)) {
-  
-     ?>
+  ?>
    <div>
-   	 <span><?=$row['title']?></span>
-   	 <span><?=$row['name']?></span>
-   	 <span><?=$row['created']?></span>
+   	 <span>제목 : <?=$row['title']?></span>
+   	 <span>글쓴이 : <?=$row['name']?></span>
+     <span>조회수  : <?=$row['hit']?></span>
    </div>
    <div>
-   	<span><?=$row['message']?></span>
-   	</div>
-      <div>
-       <button type = "button" onclick="location.href='update.php?number=<?=$row['number']?>">수정</button>
+   	  <span>내용 : <?=$row['message']?></span>
+   </div>
+   <div>
+       <button type = "button" onclick="location.href='update.php?number=<?=$row['number']?>'">수정</button>
        <button type = "button" onclick="location.href='delete.php?number=<?=$row['number']?>'">삭제</button>
-      </div>
-   	<?php 
+   </div>
+<?php 
    	} 
    	mysqli_close($conn);
-   	?>
-      <div>
+?>
+   <div>
        <button type = "button" onclick="location.href='list.php'">게시판으로이동하기</button>
-      </div>
-      
-</body>
-</html>
+   </div>
+
+<?php
+    include 'footer.php'; 
+ 
+?>
