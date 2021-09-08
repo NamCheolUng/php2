3b1b623849e41c7400bb6da9d66988889a60d4be
NamCheolUng <nam92116@naver.com>
2021-09-08 16:04:50 +0900

    Update update_process.php

--- board/update_process.php
+++ board/update_process.php
@@ -1,32 +1,27 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>Update</title>
-</head>
-<body>
-	<?php
-		 $conn = mysqli_connect("localhost", "root", "111111", "bo_table");
+<?php
+    include 'header.php';
 
-		$u_number = $_POST['number'];
-		$u_title = $_POST['title'];
-	 	$u_message = $_POST['message'];
+	$conn = mysqli_connect("localhost", "root", "111111", "bo_table");
 
-	 	$sql = "UPDATE board SET title = '$u_title', message = '$u_message' WHERE number = '$u_number'";
-	 	$result = mysqli_query($conn, $sql);
+	$u_number = $_POST['number'];
+	$u_title = $_POST['title'];
+	$u_message = $_POST['message'];
+
+	 $sql = "UPDATE board SET title = '$u_title', message = '$u_message', created = now() WHERE number = '$u_number'";
+	 $result = mysqli_query($conn, $sql);
 
 	 	if (!$result) {
 	 		echo '수정하지 못했습니다.';
 	 	}else{
-	?>
+?>
 	<script type="text/javascript">
 		alert("수정완료되었습니다.");
 		location.href="list.php";
 	</script>
 
-	<?php
+<?php
 	}
-	mysqli_close($conn); 
-	?>
-</body>
-</html>
+	 mysqli_close($conn); 
+	 include 'footer.php'; 
+?>
+
