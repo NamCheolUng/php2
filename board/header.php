ad9fa5e05ca2c41734ba929d832384e45efd5388
NamCheolUng <nam92116@naver.com>
2021-09-09 16:27:13 +0900

    Update header.php

--- board/header.php
+++ board/header.php
@@ -4,14 +4,37 @@
 	<meta charset="utf-8">
 	<title>게시판</title>
 	<style type="text/css">
-	.a {
+
+	a:link{
+		color: black;
+	}
+
+	a:visited{
+		color: black;
+	}
+
+	a { 
+		text-decoration:none;
+	}
+
+	.ta {
 		text-align: center;
 	}
 
 	table {
     margin-left:auto; 
     margin-right:auto;
-}
+	}
+
+	.re_ct{
+		color:blue;
+		/*font-weight:bold;*/
+	}
+
+	form {
+	display: inline;
+  }
+	
 	</style>
 </head>
 <body>
