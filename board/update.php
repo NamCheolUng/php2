77de76957ed4ccdcc01430fef484f9200a146417
NamCheolUng <nam92116@naver.com>
2021-09-10 16:16:29 +0900

    Update update.php

--- board/update.php
+++ board/update.php
@@ -1,36 +1,40 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>수정</title>
-</head>
-<body>
-      <?php
-        $conn = mysqli_connect("localhost", "root", "111111", "bo_table");
-
-        $up_num = $_GET['number'];
-        $sql = "SELECT * FROM board WHERE number = '$up_num'";
+<?php
+    
+     $title = "수정하기";
+    include 'header.php';
 
 
-        $result = mysqli_query($conn, $sql);
+    $conn = mysqli_connect("localhost", "root", "111111", "bo_table");
 
-		?>
-		<h1>수정화면</h1>
-		<?php
-		if($row = mysqli_fetch_array($result)){
-		?>
+    $up_num = $_GET['number'];
+    $sql = "SELECT * FROM board WHERE number = '$up_num'";
 
-		<form action="update_process.php" method="post">
-			    <input type="hidden" name="number" value="<?=$row['number']?>"> 
-				<p>글쓴이 : <?=$row['name']?></p> 
-				<p><label for="title">제목 : </label><input type="text" name="title" id="title" value="<?=$row['title']?>"></p>
-        		<p><label for="message">내용 : </label><textarea name="message" id="message" cols="30" rows="10"><?=$row['message']?></textarea></p>
-        		<input type="submit" value="수정하기">
-		</form>
 
-		<?php
+    $result = mysqli_query($conn, $sql);
+
+
+
+?>
+	<h1>수정화면</h1>
+<?php
+	if($row = mysqli_fetch_array($result)){
+?>
+
+	<form action="update_process.php" method="post">
+		<fieldset>
+    		<legend>입력사항</legend>
+			<input type="hidden" name="number" value="<?=$row['number']?>"> 
+			<p>글쓴이 <?=$row['name']?></p> 
+			<p><label for="title">제목</label><input type="text" name="title" id="title" value="<?=$row['title']?>"></p>
+        	<p><textarea id="summernote" name="message"><?=$row['message']?></textarea></p>
+        	<input type="submit" value="수정하기">
+		</fieldset>
+	</form>
+
+<?php
 		}
-		mysqli_close($conn); 
-		?>
-</body>
-</html>
+	mysqli_close($conn); 
+    include 'footer.php'; 
+
+?>
+	
