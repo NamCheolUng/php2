36a79116e0559678c119f4b5a6cca6ab15468af2
NamCheolUng <nam92116@naver.com>
2021-10-10 12:50:22 +0900

    Update ex.php

--- ex/ex.php
+++ ex/ex.php
@@ -1,5 +1,12 @@
 <?php 
-echo "<a href=\"www.naver.com\">.가.</a><br>";
+$a = 7894561230;
+echo "<a href=https://www.naver.com>가adad</a><br>";
 echo "<a href=\"www.naver.com\">'나'</a><br>";
 echo "<a href=\"www.naver.com\">다</a><br>";
+echo '<a href=https://www.naver.com>123123123</a><br>';
+echo '123'."<br>";
+echo "456".'<br>';
+echo "789".'<br>';
+echo '$a'.'<br>';
+echo "$a".'<br>';
 ?>
