c7c4caf1c6c49dacaf32c430149f2bb321f7bafd
NamCheolUng <nam92116@naver.com>
2021-11-23 17:54:41 +0900

    Update 2.php

--- calculator(end)/2.php
+++ calculator(end)/2.php
@@ -5,20 +5,42 @@ $num = $_POST['num'];
 $sum = $_POST['sum'];
 
 
+
+$ar = array('num'=>$num,
+            'sum'=>$sum
+           
+            );
+
+
  if($num != "=" && $num != "clear"){
-  	echo $num;
+  	echo json_encode($ar);
+    exit;
  } else if($num == 'clear'){
-   echo $num;
+        echo json_encode($ar);
+         exit;
  } else if($num == '='){
-    
-     $oper = substr($sum,strlen($sum)-1,strlen($sum));
+ 
+  
+   $oper = substr($sum,strlen($sum)-1,strlen($sum));
+   
+   if($oper == "+" || $oper == "-" || $oper == "/" || $oper == "*"){
+        $z = $sum.substr($sum,0,strlen($sum)-1);
+        $q = eval("echo $z;"); // undefined
+        // $q = json_encode(eval("echo $z;")); //null
 
-    if($oper == "+" || $oper == "-" || $oper == "/" || $oper == "*"){
-        $z = $sum.substr(0,strlen($sum)-1);
-        $q = eval($z);
-        echo $q;
 
- }
+        $ar['q'] = $q;
+        
+     
+         
+            echo json_encode($ar);
+            exit;
+
+     } else{
+             echo json_encode($ar);
+              exit;
+     }
+
 
  }
 
