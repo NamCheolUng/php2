c74b0b3e3eedfc1feba89e9e31d0a5d96e955a85
NamCheolUng <nam92116@naver.com>
2021-12-30 13:15:37 +0900

    Update 1.php

--- 실습2/1.php
+++ 실습2/1.php
@@ -18,50 +18,50 @@
 	<tr>
 		<td>
 			<select id="" onchange="category(this)">
-               <option value="">비목선택</option>
-               <option value="1">직접비</option>
-               <option value="2">간접비</option>
-         </select>
-        </td>
-		<td>
-		   <select name="cM" id="cateM" class="" disabled>	
-		    	  <option value="">세목선택</option>
+          <option value="">비목선택</option>
+          <option value="1">직접비</option>
+          <option value="2">간접비</option>
+      </select>
+    </td>
+	   <td>
+		   <select name="cM" id="cateM" class="p" disabled>	
+		    	<option value="">세목선택</option>
 		</td>
-      <td><input type="text" id="sm" onkeyup="check()"></td>
+    <td><input type="text" id="sm" onkeyup="check()"></td>
 		<td><input type="text" id="self" onkeyup="check()"></td>
 		<td><input type="text" id="sum"></td>
 	</tr>
 	<tr>
 		<td>
 			<select onchange="category2(this)">
-               <option value="">비목선택</option>
-               <option value="1">직접비</option>
-               <option value="2">간접비</option>
-         </select>
-        </td>
+          <option value="">비목선택</option>
+          <option value="1">직접비</option>
+          <option value="2">간접비</option>
+      </select>
+    </td>
 		<td>
-		   <select name="cM2" id="cateM2" class="" disabled>	
-		    	   <option value="">세목선택</option>
+		   <select name="cM2" id="cateM2" class="p" disabled>	
+		    	<option value="">세목선택</option>
 		</td>
-        <td><input type="text" id="sm2" onkeyup="check()"></td>
-		  <td><input type="text" id="self2" onkeyup="check()"></td>
-		  <td><input type="text" id="sum2"></td>
+    <td><input type="text" id="sm2" onkeyup="check()"></td>
+		<td><input type="text" id="self2" onkeyup="check()"></td>
+		<td><input type="text" id="sum2"></td>
 	</tr>
 	<tr>
 		<td>
 			<select onchange="category3(this)">
-               <option value="">비목선택</option>
-               <option value="1">직접비</option>
-               <option value="2">간접비</option>
-         </select>
-        </td>
+           <option value="">비목선택</option>
+           <option value="1">직접비</option>
+           <option value="2">간접비</option>
+      </select>
+    </td>
 		<td>
-		   <select name="cM3" id="cateM3" class="" disabled>	
-		    	   <option value="">세목선택</option>
+		   <select name="cM3" id="cateM3" class="p" disabled>	
+		    	 <option value="">세목선택</option>
 		</td>
-        <td><input type="text" id="sm3" onkeyup="check()"></td>
-		  <td><input type="text" id="self3" onkeyup="check()"></td>
-		  <td><input type="text" id="sum3"></td>
+    <td><input type="text" id="sm3" onkeyup="check()"></td>
+		<td><input type="text" id="self3" onkeyup="check()"></td>
+		<td><input type="text" id="sum3"></td>
 	</tr>
 	<tr>
 		<td colspan="2">합계</td>
@@ -70,10 +70,10 @@
 		<td><input type="text" id="sum4"></td>
 	</tr>
 </table>
-<div id="div" style="text-align: center; margin-top: 50px;"> 
-<button type="button" class="save">저장</button>
-<button type="button" onclick="location.href='1.php'">되돌아가자</button>
-<dvi>
+  <div id="div" style="text-align: center; margin-top: 50px;"> 
+  <button type="button" class="save">저장</button>
+  <button type="button" onclick="location.href='1.php'">되돌아가기</button>
+  <dvi>
 
 
 <script type="text/javascript">
@@ -108,137 +108,155 @@
   var sum3 = [];
  
 		function category(e){
-               a.pop();
-               arrA.pop();
-               a1.pop();
-               arrA1.pop();
-			var cateA = ['인건비(직접비)','연구활동비(직접비)'];
+      a.pop();
+      a1.pop();
+     	var cateA = ['인건비(직접비)','연구활동비(직접비)'];
 			var cateB = ['인건비(간접비)','연구활동비(간접비)'];
-		     var cateC = ['세목선택'];
+		  var cateC = ['세목선택'];
 			var target = document.getElementById("cateM");
          
 			if (e.value == 1) {
               $("select[name='cM']").attr('disabled',false);  
               var result = cateA;  
-              var result0 = cateA.join("");
-              // var result1 = cateA[1];
-              
-
-
-              if(result0 == '인건비(직접비)연구활동비(직접비)'){
-              	 
-              	  arrA.push(result0);
-              }
-
-              
               a.push('직접비');
-              // arrA.push(result0);
-
-
+         $(".p").click(function(){
+ 	                arrA.pop();
+         	      // if($("select[name='cM'] option:selected").val() == '인건비(직접비)'){
+               if($(this).val() == '인건비(직접비)'){
+         	        arrA.push('인건비(직접비)');
+         	      }
+         	      else if($("select[name='cM'] option:selected").val() == '연구활동비(직접비)'){
+         	      	arrA.push('연구활동비(직접비)');
+         	      }
+           });
          }else if (e.value == 2) {
-         	 $("select[name='cM']").attr('disabled',false);  
+         	    $("select[name='cM']").attr('disabled',false);  
          	    var result = cateB;  
-              var result0 = cateB[0];
-              var result0 = cateB[1];
-                a1.push('간접비');
-                 arrA1.push(result0);
+              a1.push('간접비');
+               
+          $(".p").click(function(){
+ 	               arrA1.pop();
+         	      if($("select[name='cM'] option:selected").val() == '인건비(간접비)'){
+         	        arrA1.push('인건비(간접비)');
+         	      }
+         	      else if($("select[name='cM'] option:selected").val() == '연구활동비(간접비)'){
+         	      	arrA1.push('연구활동비(간접비)');
+         	      }
+           });
                  
-			}else{   
-				 $("select[name='cM']").attr('disabled',true); 
-				  var result = cateC;
-			}
+			   }else{   
+				      $("select[name='cM']").attr('disabled',true); 
+				      var result = cateC;
+			   }
 
-			target.options.length = 0;
+			       target.options.length = 0;
 
-			for (var i in result){
-				var opt = document.createElement("option");
-				opt.innerHTML = result[i];
-		          target.appendChild(opt);
-			}
+		    	for (var i in result){
+				    var opt = document.createElement("option");
+			    	opt.value = result[i];
+						opt.innerHTML = result[i];
+				    target.appendChild(opt);
 
-
+		    	}
 		}
 
-	   function category2(e){
+	  function category2(e){
 	   	 b.pop();
-	   	 arrB.pop();
 	   	 b1.pop();
-	   	 arrB1.pop();
-			var cateA = ['인건비(직접비)','연구활동비(직접비)'];
+	 		var cateA = ['인건비(직접비)','연구활동비(직접비)'];
 			var cateB = ['인건비(간접비)','연구활동비(간접비)'];
-		     var cateC = ['세목선택'];
+		  var cateC = ['세목선택'];
 			var target = document.getElementById("cateM2");
          
 			if (e.value == 1) {
               $("select[name='cM2']").attr('disabled',false);    
               var result = cateA;
-              var result0 = cateA[0];
-              var result0 = cateA[1];
-
-               b.push('직접비');
-              arrB.push(result0);
-         }else if (e.value == 2) {
-         	 $("select[name='cM2']").attr('disabled',false);    
+              b.push('직접비');
+         $(".p").click(function(){
+ 	               arrB.pop();
+         	      if($("select[name='cM2'] option:selected").val() == '인건비(직접비)'){
+         	        arrB.push('인건비(직접비)');
+         	      }
+         	      else if($("select[name='cM2'] option:selected").val() == '연구활동비(직접비)'){
+         	      	arrB.push('연구활동비(직접비)');
+         	      }
+           });
+        }else if (e.value == 2) {
+         	    $("select[name='cM2']").attr('disabled',false);    
               var result = cateB;
-              var result0 = cateB[0];
-              var result0 = cateB[1];
-               b1.push('간접비');
-               arrB1.push(result0);
+              b1.push('간접비');
+         $(".p").click(function(){
+ 	               arrB1.pop();
+         	      if($("select[name='cM2'] option:selected").val() == '인건비(간접비)'){
+         	        arrB1.push('인건비(간접비)');
+         	      }
+         	      else if($("select[name='cM2'] option:selected").val() == '연구활동비(간접비)'){
+         	      	arrB1.push('연구활동비(간접비)');
+         	      }
+           });
 			}else{   
 				 $("select[name='cM2']").attr('disabled',true); 
 				  var result = cateC;
 			}
 
-			target.options.length = 0;
+			    target.options.length = 0;
 
-			for (var i in result){
-				var opt = document.createElement("option");
-				opt.innerHTML = result[i];
-		      target.appendChild(opt);
+		    	for (var i in result){
+				    var opt = document.createElement("option");
+					  opt.value = result[i];
+			    	opt.innerHTML = result[i];
+		        target.appendChild(opt);
 			}
-
-
-		}
+}
 
 		function category3(e){
 			c.pop();
-			arrC.pop();
 			c1.pop();
-			arrC1.pop();
 			var cateA = ['인건비(직접비)','연구활동비(직접비)'];
 			var cateB = ['인건비(간접비)','연구활동비(간접비)'];
-		     var cateC = ['세목선택'];
+		  var cateC = ['세목선택'];
 			var target = document.getElementById("cateM3");
          
 			if (e.value == 1) {
               $("select[name='cM3']").attr('disabled',false);    
               var result = cateA;
-              var result0 = cateA[0];
-              var result0 = cateA[1];
-               c.push('직접비');
-              arrC.push(result0);
+              c.push('직접비');
+         $(".p").click(function(){
+ 	               arrC.pop();
+         	      if($("select[name='cM3'] option:selected").val() == '인건비(직접비)'){
+         	        arrC.push('인건비(직접비)');
+                }
+         	      else if($("select[name='cM3'] option:selected").val() == '연구활동비(직접비)'){
+         	      	arrC.push('연구활동비(직접비)');
+         	      }
+           });
          }else if (e.value == 2) {
          	 $("select[name='cM3']").attr('disabled',false);    
               var result = cateB;
-              var result0 = cateB[0];
-              var result0 = cateB[1];
-                c1.push('간접비');
-              arrC1.push(result0);
+               c1.push('간접비');
+          $(".p").click(function(){
+ 	               arrC1.pop();
+         	      if($("select[name='cM3'] option:selected").val() == '인건비(간접비)'){
+         	        arrC1.push('인건비(간접비)');
+         	      }
+         	      else if($("select[name='cM3'] option:selected").val() == '연구활동비(간접비)'){
+         	      	arrC1.push('연구활동비(간접비)');
+         	      }
+           });
 			}else{   
 				 $("select[name='cM3']").attr('disabled',true); 
 				  var result = cateC;
 			}
 
-			target.options.length = 0;
+			    target.options.length = 0;
 
-			for (var i in result){
-				var opt = document.createElement("option");
-				opt.innerHTML = result[i];
-		      target.appendChild(opt);
+		    	for (var i in result){
+			    	var opt = document.createElement("option");
+					 opt.value = result[i];
+				   opt.innerHTML = result[i];
+		       target.appendChild(opt);
 			}
-
-
-		}
+}
            
       function check(){
            	var a = $('#sm').val();
@@ -256,7 +274,7 @@
            	}     
            	var c = Number(a) + Number(b);
            	sum = [];
-               sum.push(c);
+            sum.push(c);
            	$('#sum').val(c);
           
 
@@ -264,10 +282,10 @@
            	sM1 = [];
            	sM1.push(a2);   	
            	var b2 = $('#self2').val();
-           	 sF1  = []; 
-           	 sF1.push(b2); 
+           	sF1  = []; 
+           	sF1.push(b2); 
 
-              if(a2==""){
+            if(a2==""){
            		$('#sm2').val(0);
            	} 	
            	if(b2==""){
@@ -281,10 +299,10 @@
 
            	var a3 = $('#sm3').val();
            	sM2 = [];
-           	 sM2.push(a3);    	
+           	sM2.push(a3);    	
            	var b3 = $('#self3').val();
            	sF2 = [];
-           	 sF2.push(b3);	
+           	sF2.push(b3);	
 
            	if(a3==""){
            		$('#sm3').val(0);
@@ -312,26 +330,23 @@
             var c6 = c + c2 + c3;
             $('#sum4').val(c6);
             sum3 = []
-              sum3.push(c6);
+            sum3.push(c6);
           
-         }
+      }
 
          $(document).on("click",'.save',function(){
-
               var conf = confirm('저장하시겟습니까?');
               if(conf){
-              	 
-              	 var action = "2.php";
+              var action = "2.php";
             $.ajax({
                type:'POST',
                url:action,
                dataType:'json',
                data:'arrA='+arrA+'&a='+a+'&arrA1='+arrA1+'&a1='+a1+'&arrB='+arrB+'&b='+b+'&arrB1='+arrB1+'&b1='+b1+'&arrC='+arrC+'&c='+c+'&arrC1='+arrC1+'&c1='+c1+'&sM='+sM+'&sM1='+sM1+'&sM2='+sM2+'&sMS='+sMS+'&sF='+sF+'&sF1='+sF1+'&sF2='+sF2+'&sFS='+sFS+'&sum='+sum+'&sum1='+sum1+'&sum2='+sum2+'&sum3='+sum3,
                 success:function(data){
-
                  $('#del').remove();
                  $("#div").before(
-                      '<table border="1" style="text-align:center; margin-top: 50px; margin-right: auto; margin-left: auto;">' +
+               '<table border="1" style="text-align:center; margin-top: 50px; margin-right: auto; margin-left: auto;">' +
 										'<tr>' +
 											'<th>비목</th>' +
 											'<th>세목</th>' +
@@ -407,33 +422,13 @@
 											'<td>' +
 											 data['sum3'] +
 											'</td>' +
-										'</tr>' +
-							'</table>'
+						 				'</tr>' +
+							 '</table>'
                  	);
-
-
                 }
-            });
+             });
            }
-         });
-
-
-
-
-
-		 //  $('#sm').keyup(function(){
-   //              a = $(this).val();
-   //              $('#sm2').val(a);
-              
-		 // });
-
-		 //    $('#self').keyup(function(){
-   //               b = $(this).val();
-   //              $('#self2').val(b);
-              
-		 // });
-	      
-	     
+        });	     
 
 </script>
 
